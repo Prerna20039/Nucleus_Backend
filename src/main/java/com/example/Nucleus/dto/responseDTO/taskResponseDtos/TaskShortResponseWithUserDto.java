0e13ce6fa84b9378aedfbb1b8f318fd4b32c87df
Prerna20039 <prernapatankar10@gmail.com>
2026-07-10 17:29:50 +0530

    updated response dto for task

--- src/main/java/com/example/Nucleus/dto/responseDTO/taskResponseDtos/TaskShortResponseWithUserDto.java
+++ src/main/java/com/example/Nucleus/dto/responseDTO/taskResponseDtos/TaskShortResponseWithUserDto.java
@@ -8,24 +8,67 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class TaskShortResponseWithUserDto {
+    private Long id;
+
     private String title;
+
+    private String description;
+
     private TaskStatusType status;
+
     private PriorityType priority;
+
     private LocalDateTime startDate;
+
     private LocalDateTime endDate;
-    List<UserShortResponseDto> assignTo;
+
+    private String img;
+
+    private List<UserShortResponseDto> assignTo;
 
     public TaskShortResponseWithUserDto() {
     }
 
-    public TaskShortResponseWithUserDto(String title, TaskStatusType status, PriorityType priority, LocalDateTime startDate, LocalDateTime endDate, List<UserShortResponseDto> assignTo) {
+    public TaskShortResponseWithUserDto(Long id, String title,String description, TaskStatusType status, PriorityType priority, LocalDateTime startDate, LocalDateTime endDate,String img, List<UserShortResponseDto> assignTo) {
+        this.id = id;
         this.title = title;
+        this.description = description;
         this.status = status;
         this.priority = priority;
         this.startDate = startDate;
         this.endDate = endDate;
+        this.img = img;
         this.assignTo = assignTo;
     }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+
 
     public String getTitle() {
         return title;
