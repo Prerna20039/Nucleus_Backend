218692c6d47c9d68db77d658825609bb601650f8
Prerna20039 <prernapatankar10@gmail.com>
2026-07-11 04:25:31 +0530

    updated cors config

--- src/main/java/com/example/Nucleus/config/CorsConfig.java
+++ src/main/java/com/example/Nucleus/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "https://nucleus-frontend-jade.vercel.app"
          // Replace with your deployed frontend URL
         ));
 
